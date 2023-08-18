28b1cea9c2b7604ce99428ec7419af1237651fa7
Varshinis10 <varshinis54@gmail.com>
2023-08-18 16:20:53 +0530

    abcd

---
+++ src/Conn.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+public class Conn
+{
+    Connection c;
+    Statement s;
+    public Conn()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///bankManagementSystem","root","#Varsh10");
+            s = c.createStatement();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
